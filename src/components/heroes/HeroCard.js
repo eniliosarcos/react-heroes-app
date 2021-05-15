1d200b66ead849deae64608779954b0b193e311c
Enilio Sarcos <eniliosarcosdev@gmail.com>
2021-05-15 18:02:56 -0400

    cambios en el diseño de las cartas

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,39 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HeroCard.css'
 
 export const HeroCard = ({hero}) => {
     return (
-        <div className="card ms-3" style={{maxWidth:540}}>
-            <div className="row no-gutters">
-                <div className="col-md-4">
-                    <img src={`./assets/heroes/${hero.id}.jpg`} className="card-img" alt={hero.superhero} />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            {hero.superhero}
-                        </h5>
-                        <p className="card-text">
-                            {hero.alter_ego}
-                        </p>
-
+        <Link to={`./hero/${hero.id}`} className="my-card">
+        <img src={`./assets/heroes/${hero.id}.jpg`} className="img img-responsive" alt={hero.superhero}/>
+        <div className="profile-name">{hero.superhero}</div>
+        <div className="profile-position">{hero.alter_ego}</div>
+        <div className="profile-overview">
+            <div className="profile-overview">
+                <div className="row">
+                    <div className="col-ms-4">
+                        <h3>{hero.publisher}</h3>
+                        <p>First Appearance: <br />{hero.first_appearance}</p>
                         {
-                            (hero.alter_ego !== hero.characters) 
-                                && <p className="card-text">{ hero.characters }</p>
+                            (hero.alter_ego !== hero.characters)
+                            && <p>{hero.characters}</p>
                         }
-
-                        <p className="card-text">
-                            <small className="text-muted">
-                                {hero.first_appearance}
-                            </small>
-                        </p>
-
-                        <Link to={`./hero/${hero.id}`}>
-                            Mas...
-                        </Link>
                     </div>
                 </div>
             </div>
         </div>
+    </Link>
     )
 }
